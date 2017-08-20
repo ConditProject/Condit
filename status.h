a81e73d3fdb2e84e906246f88f6b0fc43580cbe0
Samiur Khan <samiurkh1n@gmail.com>
2017-08-20 16:34:10 -0400

    Updated header file with more documentation

--- status.h
+++ status.h
@@ -1,3 +1,29 @@
+// Status, a status-tracking object for C++ binaries
+//
+// An instance of the Status class holds two pieces of data.
+//  1. The state of the program (ex: Ok, Invalid, Unknown, NotFound, etc...)
+//  2. A description of the state
+//
+// The Status object packages the information together to allow for robust
+//  software (where the paths an executable can take are well defined) that
+//  is more readable. The status object is an alternative to using integers
+//  or bools for tracking the state of a program.
+//
+// Use cases:
+// 
+// (_) Status can be the return type of a program -
+// Status SomeComputation(...) {
+//   do_some_computing()
+//   something something 42
+//   return Status::OkStatus();
+// }
+//
+// (_) Status can be used in program decision making -
+// Status program_state;
+// if (program_state) {
+//   do_this_set_of_computation();
+// }
+
 #ifndef STATUS_H_
 #define STATUS_H_
 
@@ -26,6 +52,7 @@ public:
 
   enum State State() const { return state_; }
   std::string Message() const { return message_; }
+  
   operator bool() const { return state_ == OK; }
 private:
   enum State state_;
